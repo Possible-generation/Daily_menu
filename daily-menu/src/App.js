9998e49c6bea7d836d18c9b3e87a5414016a12e0
Possible <ajigbotoshojoshua2000@gmail.com>
2024-02-23 19:48:58 +0100

    setting up landing page

--- daily-menu/src/App.js
+++ daily-menu/src/App.js
@@ -1,13 +1,32 @@
-import './App.css';
+import './index.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Landing from './page/Landing';
+import SignUp from '../src/page/Registration';
+import Login from '../src/page/Login';
+import Dashboard from './page/Dashboard';
+import 'react-toastify/dist/ReactToastify.css';
+import { ShopContextProvider } from './Context/shop-context';
+import Cart from './page/Cart';
+import Checkout from './page/Checkout';
+import CartOrder from './page/Order';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-         DAILY FOOD MENU
-        </p>
-      </header>
+    <div>
+      <ShopContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/Registration' element={<SignUp />} />
+          <Route path='/Login' element={<Login />} />
+          <Route path='/Dashboard' element={<Dashboard />} />
+          <Route path='/Cart' element={<Cart />} />
+          <Route path='/Checkout' element={<Checkout />}  />
+          <Route path='/CartOrder' element={<CartOrder />} />
+        </Routes>
+      </BrowserRouter>
+      </ShopContextProvider>
     </div>
   );
 }
